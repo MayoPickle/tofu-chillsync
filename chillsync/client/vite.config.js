7915190ce5bc246f97454eeb45a962963eca5f00
Robert Yu <me@boyangyu.cn>
2025-03-29 11:23:19 +0000

    Update Vite configuration to allow external access and specify allowed hosts. Set strict port enforcement for the development server.

--- chillsync/client/vite.config.js
+++ chillsync/client/vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",  // 允许外部访问
     port: 3000,
+    strictPort: true,
+    allowedHosts: ["chillsync.yudoufu.org"], // 允许的域名
     proxy: {
       '/api': {
         target: 'http://localhost:5010',
@@ -21,4 +24,4 @@ export default defineConfig({
       },
     },
   },
-}) 
+})
